c0a15cc61f29d3c4dfbd1a1a34541d3dc17c515f
Tania Roussou <57332815+taniaroussou@users.noreply.github.com>
2021-03-01 20:09:29 +0200

    Update main.c

--- Graph/main.c
+++ Graph/main.c
@@ -17,55 +17,55 @@ void visit_string(LLList list, LLNode node) {
 */
 
 int main(){
-    UGGraph *graph = UGCreate();
-    graph = UGAddVertex(graph,"foo");
-    graph = UGAddVertex(graph,"bar");
-    graph = UGAddVertex(graph,"abc");
-	graph = UGAddVertex(graph,"izi");
-	graph = UGAddVertex(graph,"piz");
-    UGAddEdge(graph,"foo","bar");
-	UGAddEdge(graph,"foo","piz");
-	UGAddEdge(graph,"foo","izi");
-	UGAddEdge(graph,"piz","bar");
-	UGAddEdge(graph,"izi","bar");
-	UGAddEdge(graph,"izi","abc");
-    LLList list = UGGetAdjacent(graph,"foo");
+	UGGraph *graph = UGCreate();
+	graph = UGAddVertex(graph, "foo");
+	graph = UGAddVertex(graph, "bar");
+	graph = UGAddVertex(graph, "abc");
+	graph = UGAddVertex(graph, "izi");
+	graph = UGAddVertex(graph, "piz");
+	UGAddEdge(graph, "foo", "bar");
+	UGAddEdge(graph, "foo", "piz");
+	UGAddEdge(graph, "foo", "izi");
+	UGAddEdge(graph, "piz", "bar");
+	UGAddEdge(graph, "izi", "bar");
+	UGAddEdge(graph, "izi", "abc");
+	LLList list = UGGetAdjacent(graph, "foo");
 	printf("List of vertices adjacent to foo is : ");
-    LLVisit(list,visit_string);
+	LLVisit(list, visit_string);
 
 	printf("\n");
-    //UGRemoveVertex(graph,"foo");
-    //UGRemoveEdge(graph,"foo","bar");
-	list = UGGetAdjacent(graph,"bar");
+	//UGRemoveVertex(graph,"foo");
+	//UGRemoveEdge(graph,"foo","bar");
+	list = UGGetAdjacent(graph, "bar");
 	printf("List of vertices adjacent to bar is : ");
-    LLVisit(list,visit_string);
+	LLVisit(list, visit_string);
 
 	printf("\n");
-	list = UGGetAdjacent(graph,"abc");
+	list = UGGetAdjacent(graph, "abc");
 	printf("List of vertices adjacent to abc is : ");
-    LLVisit(list,visit_string);
+	LLVisit(list, visit_string);
 
 	printf("\n");
-	list = UGGetAdjacent(graph,"izi");
+	list = UGGetAdjacent(graph, "izi");
 	printf("List of vertices adjacent to izi is : ");
-    LLVisit(list,visit_string);
+	LLVisit(list, visit_string);
 
 	printf("\n");
-	list = UGGetAdjacent(graph,"piz");
+	list = UGGetAdjacent(graph, "piz");
 	printf("List of vertices adjacent to piz is : ");
-    LLVisit(list,visit_string);
+	LLVisit(list, visit_string);
 
 	printf("\n");
-	LLList path = UGShortestPath(graph,"abc","piz");
-	if (LLLength(path)==0)
+	LLList path = UGShortestPath(graph, "abc", "piz");
+	if (LLLength(path) == 0)
 		printf("There's no path\n");
 	else{
 		printf("Path is: ");
-		LLVisit(path,visit_string);
+		LLVisit(path, visit_string);
 	}
 
 	printf("\n");
 	LLDestroy(path);
 	UGDestroy(graph);
-    return 0;
+	return 0;
 }
